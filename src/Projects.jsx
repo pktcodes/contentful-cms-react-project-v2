797c909d26d2ee42aca2aa8a571b3e5dc9b788a2
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-10-15 00:10:43 -0500

    Render Projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,9 +3,39 @@ import useFetchProjects from './fetchProjects';
 const Projects = () => {
   const { isLoading, projects } = useFetchProjects();
 
-  console.log(isLoading, projects);
+  if (isLoading) {
+    return (
+      <section className="projects">
+        <h2 className="title">loading...</h2>
+      </section>
+    );
+  }
 
-  return <h2>Projects</h2>;
+  return (
+    <section className="projects">
+      <div className="title">
+        <h2>projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        {projects.map((project) => {
+          const { id, img, title, url } = project;
+          return (
+            <a
+              key={id}
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+              className="project"
+            >
+              <img src={img} alt={title} className="img" />
+              <h5>{title}</h5>
+            </a>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
